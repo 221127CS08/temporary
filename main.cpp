e53f083ece2e85f348cf9c3feb4e7fa76a6c8951
221127 CS08 <113343787+221127CS08@users.noreply.github.com>
2022-10-12 10:40:20 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  loop
+//
+//  Created by Mukhammadrasul Akramhujaev on 10/10/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 1;
+    for (i=1; i<100; i+=2 ) {
+        cout << i << " ";
+    }
+    cout << i << endl;
+    return 0;
+}
