7da348da4726e6330f2113733cc120e68ea8aee4
221127 CS08 <113343787+221127CS08@users.noreply.github.com>
2022-10-19 11:34:11 +0500

    Update and rename main.cpp to functions-6.cpp

---
+++ functions-6.cpp
@@ -0,0 +1,81 @@
+//
+//  main.cpp
+//  loop
+//
+//  Created by Mukhammadrasul Akramhujaev on 10/10/22.
+//
+
+//PROBLEM-1
+//#include <iostream>
+
+// using namespace std;
+
+// int sum(int a, int b);
+
+// int main() {
+//     int a, b;
+//     cout << "input two numbers -> ";
+//     cin >> a >> b;
+    
+//     cout << "the result is " << sum(a, b) << endl;
+    
+//     return 0;
+// }
+
+// int sum(int a, int b) {
+//     return a + b;
+// }
+
+//PROBLEM-2
+
+// #include <iostream>
+
+// using namespace std;
+
+// int sum(int a, int b);
+
+// int main() {
+//     int a, b;
+//     cout << "input two numbers -> ";
+//     cin >> a >> b;
+    
+//     cout << "the product of " << a << " and " << b << " is " << sum(a, b) << endl;
+    
+//     return 0;
+// }
+
+// int sum(int a, int b) {
+//     return a * b;
+// }
+
+//PROBLEM-3
+
+// #include <iostream>
+
+// using namespace std;
+
+// float area(float radius);
+// float circumference(float radius);
+
+// int main() {
+//     float radius;
+    
+//     cout << "radius -> ";
+//     cin >> radius;
+    
+//     cout << "area is " << area(radius) << endl;
+//     cout << "circumference is " << circumference(radius) << endl;
+    
+//     return 0;
+// }
+
+// float area(float radius) {
+//     return 3.14 * radius * radius;
+// }
+
+// float circumference(float radius) {
+//     return 2 * 3.14 * radius;
+// }
+
+//PROBLEM-4
+
